b165136934546afdea2e1d04fd9c327db53a6d8e
SwathiHarshad <swathi3197@gmail.com>
2020-08-17 12:39:48 +0100

    Build files

---
+++ build/precache-manifest.7f21798819bf568b86865730a7014274.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd37cf551bff6a6fec42f6e2dc084ef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a502c6bb6735dfcfcdc",
+    "url": "/static/css/main.274baa5b.chunk.css"
+  },
+  {
+    "revision": "1b0c124ac6d622d5fe4e",
+    "url": "/static/js/2.89de3074.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89de3074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a502c6bb6735dfcfcdc",
+    "url": "/static/js/main.24361f59.chunk.js"
+  },
+  {
+    "revision": "3e0ff5ccd907a66b8bf0",
+    "url": "/static/js/runtime-main.e02b7939.js"
+  },
+  {
+    "revision": "64618bafa6a6e13258c9540b2a9ab0fa",
+    "url": "/static/media/arrow_circle_down-24px.64618baf.svg"
+  },
+  {
+    "revision": "9803a6838e6c5c1e910bb6dd8651d3fd",
+    "url": "/static/media/ediflo.9803a683.png"
+  },
+  {
+    "revision": "db203fa0d82a7a8176972d370a375d3d",
+    "url": "/static/media/filter_list-24px.db203fa0.svg"
+  },
+  {
+    "revision": "d01620ce6fba39671a06391cef47c1f9",
+    "url": "/static/media/hourglass_bottom-24px.d01620ce.svg"
+  },
+  {
+    "revision": "f05a6e4c06ffee7b6c1e0d8fd26007bb",
+    "url": "/static/media/hourglass_top-24px.f05a6e4c.svg"
+  },
+  {
+    "revision": "df683721c345008b304e86104ad02ed0",
+    "url": "/static/media/sentiment_very_dissatisfied-24px.df683721.svg"
+  }
+]);
